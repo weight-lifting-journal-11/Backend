e8a71a395aa4722a94fac1065223734a80485fe4
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-05 14:42:28 -0500

    added users table

---
+++ data/migrations/20200105140943_users_table.js
@@ -0,0 +1,22 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+
+        users
+            .string('username', 128)
+            .notNullable()
+            .unique();
+
+        users
+            .string('password', 128)
+            .notNullable();
+
+        users.string('email')
+            .unique()
+            .notNullable();
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+};
