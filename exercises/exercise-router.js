3aeaf28e37c1541d7a6a710756d27e031b642771
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-01-06 10:03:02 -0500

    post and get all are working

--- exercises/exercise-router.js
+++ exercises/exercise-router.js
@@ -18,4 +18,36 @@ router.get('/',
             })
     });
 
+router.post('/',
+    // restricted, 
+    (req, res) => {
+        let newExercise = req.body;
+        if (!newExercise.name) {
+            res.status(422).json({ message: "Missing: name" })
+        }
+        if (!newExercise.weight) {
+            res.status(422).json({ message: "Missing: weight" })
+        }
+        if (!newExercise.reps) {
+            res.status(422).json({ message: "Missing: reps" })
+        }
+        if (!newExercise.sets) {
+            res.status(422).json({ message: "Missing: sets" })
+        }
+        if (!newExercise.journalId) {
+            res.status(422).json({ message: "Missing: journalId" })
+        }
+        if (!newExercise.userId) {
+            res.status(422).json({ message: "Missing: userId" })
+        }
+        Ex.add(newExercise)
+            .then(item => {
+                res.status(201).json(item);
+            })
+            .catch(err => {
+                console.log('Ex route--------->', err)
+                res.status(500).json(err)
+            })
+    })
+
 module.exports = router;
